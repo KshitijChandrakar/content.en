f489c39ca8b286f8a308be4a6a725992bf81819f
Kshitij_Chandrakar <kshitijchandrakar2019@gmail.com>
2025-01-21 15:42:33 +0530

    Pushing-content

---
+++ Notes/Semester 4/Whoops/Experiment 2/SimpleInterest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class SimpleInterest{
+  public static void main (String[] args){
+    Scanner in = new Scanner(System.in);  // Create a Scanner object
+      System.out.print("Enter Principle Amount: ");
+      float Principle = in.nextFloat();
+      System.out.print("Enter Interest in %: ");
+      float Interest = in.nextFloat();
+      System.out.print("Enter Time: ");
+      float Time = in.nextFloat();
+      float Amount = Interest * Time * Principle / 100;
+      System.out.println("Final Amount is " + Amount);
+  }
+}
---
+++ Notes/Semester 4/Whoops/Experiment1.java
@@ -0,0 +1,6 @@
+class Experiment1{
+  public static void main (String[] args){
+      System.out.println("My name is Kshitij Chandrakar");
+      System.out.println("I Live in Evara Homes, Dehradun");
+  }
+}
