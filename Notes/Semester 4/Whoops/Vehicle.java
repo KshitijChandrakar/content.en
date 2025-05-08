dad96c9153961193bfe2a9ebee38f5b1d2cdd6b7
Kshitij_Chandrakar <kshitijchandrakar2019@gmail.com>
2025-05-08 16:15:43 +0530

    "Pushing Content"

---
+++ Notes/Semester 4/Whoops/Vehicle.java
@@ -0,0 +1,15 @@
+ class Vehicle{
+ int speed=50; // parent instance variable
+ }
+
+class Bike extends Vehicle{
+ int speed=100; // child instance variable
+
+void display(){
+ System.out.println(speed);
+}
+public static void main(String args[]){
+Bike b=new Bike();
+b.display();
+}
+}
